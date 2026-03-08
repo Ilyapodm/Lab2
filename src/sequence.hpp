ea3e2d020fbe9353c683c7ffe42edf8ede0f9169
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-03-08 11:41:43 +0300

    class Bit is done

--- src/sequence.hpp
+++ src/sequence.hpp
@@ -116,7 +116,49 @@ public:
 
 };
 
-// TODO Bit последовательность
+class Bit {
+public:
+    Bit() : value{false} {}
+    Bit(bool value) : value{value} {}
+    Bit(int value) : value{value != 0} {}
+
+    // AND
+    Bit operator&(const Bit& other) const {
+        return Bit(value && other.value);
+    }
+
+    // OR
+    Bit operator|(const Bit& other) const {
+        return Bit(value || other.value);
+    }
+
+    // XOR
+    Bit operator^(const Bit& other) const {
+        return Bit(value != other.value);
+    }
+
+    // NOT
+    Bit operator~() const {
+        return Bit(!value);
+    }
+
+    bool operator==(const Bit& other) const {
+        return value == other.value;
+    }
+
+    bool operator!=(const Bit& other) const {
+        return value != other.value;
+    }
+
+private:
+    bool value;
+};
+
+class BitSequence {
+public:
+    
+private:
+};
 
 #include "sequence.tpp"
 
